5c6b6210d8476c95fb1b47f5e5e882339a037a07
victorrpf <victor.rpf@gmail.com>
2025-10-21 00:24:44 +0200

    Add comments to clarify array operations in 05_arrays.py

--- UD2/05_arrays.py
+++ UD2/05_arrays.py
@@ -1,4 +1,16 @@
+# Declarar un array (lista) de frutas
 frutas = ["plátano", "fresa", "cereza"]
 print("Lista de frutas:", frutas)
+
+# Añadir un elemento al array
 frutas.append("naranja")
 print("Después de append:", frutas)
+
+# Acceder a un elemento específico del array
+print("Cuarta fruta:", frutas[3])
+
+# Añadir un número a la lista de frutas
+numero = 1
+frutas.append(numero)
+print("Después de añadir un número:", frutas)
+# en Python las listas no están “tipadas” por elemento, son contenedores dinámicos y pueden guardar valores de cualquier tipo
