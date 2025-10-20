8ea56022b37a3628f80826e3570b60d73519f145
victorrpf <victor.rpf@gmail.com>
2025-10-21 01:06:55 +0200

    Implement nota_media function to calculate average grade and status

--- UD2/09_funciones.py
+++ UD2/09_funciones.py
@@ -1,4 +1,10 @@
-def saludo(nombre):
-    return f"Hola, {nombre}!"
+# Función para calcular la nota media de tres calificaciones
+def nota_media(a, b, c):
+    media = (a + b + c) / 3.0
+    estado = 'aprobado' if media >= 5 else 'suspenso'
+    return media, estado # Devuelve una tupla (media, estado)
 
-print(saludo("Clase"))
+print("Victor:", nota_media(5, 10, 7))
+print("María:", nota_media(6, 9, 4))
+print("Alberto:", nota_media(4, 4, 5))
+
