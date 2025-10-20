f79372fe39da93ebb0ca49c9ca259653b8a378f8
victorrpf <victor.rpf@gmail.com>
2025-10-20 23:56:21 +0200

    Refactor comments in 03_palabras_reservadas.py for clarity and consistency

--- UD2/03_palabras_reservadas.py
+++ UD2/03_palabras_reservadas.py
@@ -1,3 +1,7 @@
 import keyword
 
 print("Palabras reservadas en Python:", keyword.kwlist)
+
+# Ejemplo de uso de algunas palabras reservadas
+#False = True  # Esto generará un error porque 'False' es una palabra reservada
+#print("El valor de False es:", False)
