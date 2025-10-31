991a1c816b69ab197f5133a477d5994190b07af5
victorrpf <victor.rpf@gmail.com>
2025-10-31 10:12:09 +0100

    Refactor variable names for clarity in grade classification script

--- UD2/13_switch.py
+++ UD2/13_switch.py
@@ -1,13 +1,13 @@
 # Clasifica una nota numérica usando match-case (lo que en otros legunajes se llamaría switch)
 
-nota_texto = float(input("Introduce una nota entre 0 y 10: "))
+nota = float(input("Introduce una nota entre 0 y 10: "))
 
-match nota_texto:
-    case n if n < 0 or n > 10:
+match nota:
+    case nota if nota < 0 or nota > 10:
         print("La nota debe estar entre 0 y 10.")
-    case n if n < 5:
+    case nota if nota < 5: # Si la nota es menor que 5
         print("Suspenso")
-    case n if n < 9:
+    case nota if nota < 9: # Si la nota es menor que 9
         print("Aprobado")
-    case _:
+    case _: # Si no se ha cumplido ninguna de las anteriores condiciones debe ser sobresaliente
         print("Sobresaliente")
