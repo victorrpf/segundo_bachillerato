cc771feccb5ea995b0468a86ce6e3337ab1adf0c
victorrpf <victor.rpf@gmail.com>
2025-11-06 23:59:37 +0100

    Implement user data collection and display using a dictionary

---
+++ UD2/15_diccionario.py
@@ -0,0 +1,30 @@
+# Creamos el diccionario con listas vacías en su interior
+usuarios = {
+    "nombres": [],
+    "identificaciones": []
+}
+
+# Definimos un tamaño para las listas del diccionario
+# Lo puedes cambiar si quieres
+tamaño = 3
+
+# Leemos los datos y los agregamos a el diccionario
+for i in range(tamaño):
+    print("Ingrese los datos de la persona", i + 1)
+    nombre = input("Nombre: ")
+    identificación = input("Identificación: ")
+
+    # La primera lista es para los nombres
+    usuarios["nombres"].append(nombre)
+
+    # La segunda lista es para las identificaciones
+    usuarios["identificaciones"].append(identificación)
+
+# Agregamos una línea en blanco antes de mostrar los datos
+print()
+
+# Ahora mostremos los valores en el diccionario
+for i in range(tamaño):
+    print("Mostrando los datos de la persona", i + 1)
+    print("Nombre:", usuarios["nombres"][i])
+    print("Identificación:", usuarios["identificaciones"][i])
