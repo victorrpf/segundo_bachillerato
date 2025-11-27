a3d76753774eff0d73624abb6c232e41bbfafec9
victorrpf <victor.rpf@gmail.com>
2025-11-27 10:05:46 +0100

    Agregar archivos para manejar precios en formato JSON: crear y actualizar precios cuando tenemos un array en el JSON

---
+++ UD3/anexar_json_array.py
@@ -0,0 +1,26 @@
+# Importar el módulo json para trabajar con archivos JSON
+import json
+
+# Leer el contenido actual del archivo JSON
+with open('precios.json', 'r') as file:
+    precios = json.load(file)
+
+# Nuevos precios que queremos agregar al diccionario existente
+nuevo_precio = {
+    "id": 4,
+    "precio_4": 400,
+}
+
+# Actualizar el diccionario con las nuevas entradas
+precios["lista"].append(nuevo_precio)
+
+# Guardar nuevamente el diccionario completo en el archivo JSON
+with open('precios.json', 'w') as file:
+    json.dump(precios, file)
+print("Nuevos precios agregados al archivo 'precios.json' con éxito.")
+
+# Modos de escritura de archivos:
+# 'r' - lectura (predeterminado) 
+# 'w' - escritura (sobrescribe el archivo si ya existe)
+# 'a' - anexar (agrega contenido al final del archivo si ya existe)
+# 'x' - creación exclusiva (crea un archivo nuevo, falla si el archivo ya existe)
