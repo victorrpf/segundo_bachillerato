6000ca098eff6301bc1ebb3b2b66d9b759c7c697
victorrpf <victor.rpf@gmail.com>
2026-01-16 01:40:22 +0100

    Refactor encryption function and improve code clarity in cifrado.py

--- UD5/cifrado.py
+++ UD5/cifrado.py
@@ -1,18 +1,23 @@
-# Para codificación y decodificación en base64 que es un tipo de cifrado sencillo
+# Importar la librería base64 para codificación y decodificación
 import base64
 
-# Función lambda para realizar una operación XOR entre los datos y la clave
-# La operación XOR es una técnica básica de cifrado simétrico
-xor = lambda d,k: bytes(b ^ k[i % len(k)] for i,b in enumerate(d))
+# Función de mezcla el mensaje con la clave (cifrar).
+# Cada byte del mensaje se combina con un byte de la clave usando XOR.
+def mezcla(data, key):
+    return bytes(b ^ key[i % len(key)] for i, b in enumerate(data))
 
-m = input("Mensaje: ").encode()
+# Solicitar al usuario el mensaje y la clave
+mensaje = input("Mensaje: ").encode()
 clave = input("Clave: ").encode()
 if not clave:
-	raise SystemExit("La clave no puede estar vacía")
+    raise SystemExit("La clave no puede estar vacía")
 
-c = xor(m, clave)
+# Cifrar el mensaje
+c = mezcla(mensaje, clave)
+
+# Codificar el mensaje cifrado en base64 para facilitar su manejo
 token = base64.urlsafe_b64encode(c).decode()
 print("Cifrado:", token)
 
-# El mensaje cifrado se puede descifrar
-print("Descifrado:", xor(base64.urlsafe_b64decode(token), clave).decode())
+# Descifrar el mensaje que no sería posible con hash
+print("Descifrado:", mezcla(base64.urlsafe_b64decode(token), clave).decode())
